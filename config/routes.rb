e20f96daeaac9f5d0d9f36af8c0da625814791c7
angusscutt <angus.scutt@gmail.com>
2016-11-30 14:13:23 +1100

    Major changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :contacts, only: [:new, :create]
 
 root 'pages#index'
+
+get '/USA' => 'pages#USA'
+
+get '/yosemite' => 'pages#yosemite'
 
 end
